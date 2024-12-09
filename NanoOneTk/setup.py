69fdcf6ea0373845f2f125315ec8896fdfe1bace
keeyousha <59442354+keeyousha@users.noreply.github.com>
2024-12-10 01:11:56 +0300

    Added docs and fixed setup.py (added main protection)

--- NanoOneTk/setup.py
+++ NanoOneTk/setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="NanoOneTk",
-    version="0.1.0",
-    description="Lab №2, group NanoOne",
-    author="Kirill Taran, Maxim Shpak, Stas Berezhnitskiy",
-    packages=find_packages(),
-    install_requires=[],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.9',
-)
+if __name__ == "__main__":
+    setup(
+        name="NanoOneTk",
+        version="0.1.0",
+        description="Lab №2, group NanoOne",
+        author="Kirill Taran, Maxim Shpak, Stas Berezhnitskiy",
+        packages=find_packages(),
+        install_requires=[],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.9',
+    )
