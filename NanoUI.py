8367b3c5fc0651531e4666177eed81cb938733be
Lurik <lurik720@gmail.com>
2024-12-09 21:36:18 +0300

    update NanoUI

--- NanoUI.py
+++ NanoUI.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-from NanoOneTk import NanoLabeledEntry, NanoMoodIndicator
+from NanoOneTk import NanoLabeledEntry, NanoMoodIndicator, NanoJoke
 
 
 root = tk.Tk()
@@ -11,8 +11,12 @@ root.geometry("500x500")
 nano_labeled_entry = NanoLabeledEntry(root)
 nano_labeled_entry.pack()
 
+nano_joke = NanoJoke(root)
+nano_joke.pack()
+
 nano_mood_indicator = NanoMoodIndicator(root)
 nano_mood_indicator.pack()
 
 
 root.mainloop()
+
