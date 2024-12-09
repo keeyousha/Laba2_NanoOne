7984823b1307c9366a9f2053cd91a266124d3346
Lurik <lurik720@gmail.com>
2024-12-09 19:53:14 +0300

    add NanoMoodindicator and tests

--- NanoUI.py
+++ NanoUI.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
-from NanoOneTk import NanoLabeledEntry
+from NanoOneTk import NanoLabeledEntry, NanoMoodIndicator
+
 
 root = tk.Tk()
 
@@ -9,5 +10,8 @@ root.geometry("500x500")
 
 nano_labeled_entry = NanoLabeledEntry(root)
 nano_labeled_entry.pack()
+
+nano_mood_indicator = NanoMoodIndicator(root)
+nano_mood_indicator.pack()
 
 root.mainloop()
---
+++ tests/test_NanoModlindicator.py
@@ -0,0 +1,45 @@
+import unittest
+import tkinter as tk
+from NanoOneTk import NanoMoodIndicator  
+
+class TestNanoMoodIndicator(unittest.TestCase):
+    def setUp(self):
+        self.root = tk.Tk()
+        self.mood_indicator = NanoMoodIndicator(master=self.root)
+        self.mood_indicator.pack()
+
+    def tearDown(self):
+        self.root.destroy()
+
+    def test_initial_mood(self):
+        self.assertEqual(self.mood_indicator.mood_status.cget("text"), "Настроение: Не определено")
+        self.assertEqual(self.mood_indicator.mood_status.cget("fg"), "gray")
+
+    def test_change_mood(self):
+        self.mood_indicator.change_mood()
+        mood_text = self.mood_indicator.mood_status.cget("text")
+        self.assertTrue(mood_text.startswith("Настроение: "))       
+        mood_value = mood_text.split(": ")[1]
+        self.assertIn(mood_value, self.mood_indicator.moods)  
+
+    def test_mood_color(self):
+        for mood in self.mood_indicator.moods:
+            self.mood_indicator.mood_status.config(text=f"Настроение: {mood}")          
+            self.mood_indicator.change_mood()  
+            self.root.update()  
+            current_mood = self.mood_indicator.mood_status.cget("text").split(": ")[1]
+            if current_mood == "Счастлив!":
+                self.assertEqual(self.mood_indicator.mood_status.cget("fg"), "yellow")
+            elif current_mood == "Грустный...":
+                self.assertEqual(self.mood_indicator.mood_status.cget("fg"), "blue")
+            elif current_mood == "Злой!":
+                self.assertEqual(self.mood_indicator.mood_status.cget("fg"), "red")
+            elif current_mood == "Устал...":
+                self.assertEqual(self.mood_indicator.mood_status.cget("fg"), "gray")
+            elif current_mood == "Энергичный!":
+                self.assertEqual(self.mood_indicator.mood_status.cget("fg"), "green")
+            else:
+                self.assertEqual(self.mood_indicator.mood_status.cget("fg"), "purple")
+
+if __name__ == "__main__":
+    unittest.main()
